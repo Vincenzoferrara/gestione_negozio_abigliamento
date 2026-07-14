e5ebe657fcd062e7cdb7c73ca3f60969e45f3b32
Vincenzoferrara <vincy.fer2001@gmail.com>
2026-07-14 23:03:34 +0200

    ci: allinea target JVM Android

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,6 +26,15 @@ android {
         versionName = flutter.versionName
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
     signingConfigs {
         create("release") {
             if (keystorePropertiesFile.exists()) {
